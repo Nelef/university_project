4e811ad8dda22efe1824b0f07028334f774e7b2d
jackjackE <tmddjs210@naver.com>
2021-02-10 23:11:37 +0900

    코루틴 스타터프로젝트 first commit

---
+++ courtines-study/CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.anushka.coroutinesdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCount.setOnClickListener {
+            tvCount.text = count++.toString()
+        }
+        btnDownloadUserData.setOnClickListener {
+            downloadUserData()
+        }
+    }
+
+    private fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+        }
+    }
+}
