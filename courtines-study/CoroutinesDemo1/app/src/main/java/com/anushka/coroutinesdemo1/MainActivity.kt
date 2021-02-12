3c98f24dd0cbeef3eb2e5f8806b417b7f97094a7
jackjackE <tmddjs210@naver.com>
2021-02-12 19:39:17 +0900

    48. Async & Await

--- courtines-study/CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
+++ courtines-study/CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.anushka.coroutinesdemo1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
@@ -11,6 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(IO).launch {
+            Log.i("MyTag", "CoroutineScope Start")
+            val stock1 = async { getStock1() }
+            val stock2 = async { getStock2() }
+            val total = stock1.await() + stock2.await()
+            Log.i("MyTag", "TOTAL -> $total")
+        }
 
         btnCount.setOnClickListener {
             tvCount.text = count++.toString()
@@ -30,4 +40,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+}
+
+private suspend fun getStock1(): Int {
+    delay(10000)
+    Log.i("MyTag", "sotck 1 returned")
+    return 55000
+}
+
+private suspend fun getStock2(): Int {
+    delay(8000)
+    Log.i("MyTag", "sotck 2 returned")
+    return 35000
 }
